c00565d5e0e832aad56dfa82ca9e54221db9f999
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2024-04-17 19:39:53 +1000

    Add files via upload

--- TruscottWatters.py
+++ TruscottWatters.py
@@ -9,7 +9,6 @@ class Unsolved_Rubiks:
 	def __init__(self, state, moves):
 		self.state = state.copy()
 		self.moves = moves
-		self.moves = []
 	def is_solved(self):
 		solved_state = [["R", "R", "R", "R"], ["G", "G", "G", "G"], ["O", "O", "O", "O"], ["B", "B", "B", "B"], ["Y", "Y", "Y", "Y"], ["W", "W", "W", "W"]]
 		if self.state == solved_state:
@@ -36,8 +35,9 @@ class Unsolved_Rubiks:
 		tD[1], tD[3] = prev_B[0], prev_B[1]
 		tB[0], tB[2] = prev_U[3], prev_U[1]
 		new_matrix = [tL, tF, tR, tB, tU, tD]
-#		self.moves.append("R")
-		new_state = Unsolved_Rubiks(new_matrix, self.moves.copy())
+		n = self.moves.copy()
+		n.append("R")
+		new_state = Unsolved_Rubiks(new_matrix, n)
 		return new_state
 	def R_inv(self):
 		for n in range(0, 3, 1):
@@ -66,8 +66,10 @@ class Unsolved_Rubiks:
 		tU[0], tU[2] = prev_F[0], prev_F[2]
 		tD[0], tD[2] = prev_B[3], prev_B[1]
 		new_matrix = [tL, tF, tR, tB, tU, tD]
+		n = self.moves.copy()
+		n.append("L")
 #		self.moves.append("L")
-		new_state = Unsolved_Rubiks(new_matrix, self.moves.copy())
+		new_state = Unsolved_Rubiks(new_matrix, n)
 		return new_state
 	def L_inv(self):
 		for n in range(0, 3, 1):
@@ -97,8 +99,10 @@ class Unsolved_Rubiks:
 		tD[0], tD[1] = prev_R[2], prev_R[0]
 		tR[2], tR[0] = prev_U[3], prev_U[2]
 		new_matrix = [tL, tF, tR, tB, tU, tD]
+		n = self.moves.copy()
+		n.append("F")
 #		self.moves.append("F")
-		new_state = Unsolved_Rubiks(new_matrix, self.moves.copy())
+		new_state = Unsolved_Rubiks(new_matrix, n)
 		return new_state
 	def F_inv(self):
 		for n in range(0, 3):
@@ -128,8 +132,10 @@ class Unsolved_Rubiks:
 		tD[2], tD[3] = prev_R[3], prev_R[1]
 		tR[1], tR[3] = prev_U[0], prev_U[1]
 		new_matrix = [tL, tF, tR, tB, tU, tD]
+		n = self.moves.copy()
+		n.append("B")
 #		self.moves.append("B")
-		new_state = Unsolved_Rubiks(new_matrix, self.moves.copy())
+		new_state = Unsolved_Rubiks(new_matrix, n)
 		return new_state
 	def B_inv(self):
 		for n in range(0, 3, 1):
@@ -159,8 +165,10 @@ class Unsolved_Rubiks:
 		tR[0], tR[1] = prev_F[0], prev_F[1]
 		tF[0], tF[1] = prev_L[0], prev_L[1]
 		new_matrix = [tL, tF, tR, tB, tU, tD]
+		n = self.moves.copy()
+		n.append("U")
 #		self.moves.append("U")
-		new_state = Unsolved_Rubiks(new_matrix, self.moves.copy())
+		new_state = Unsolved_Rubiks(new_matrix, n)
 		return new_state
 	def U2(self):
 		for n in range(0, 2):
@@ -190,8 +198,9 @@ class Unsolved_Rubiks:
 		tR[2], tR[3] = prev_F[2], prev_F[3]
 		tF[2], tF[3] = prev_L[2], prev_L[3]
 		new_matrix = [tL, tF, tR, tB, tU, tD]
-#		self.moves.append("D")
-		new_state = Unsolved_Rubiks(new_matrix, self.moves.copy())
+		n = self.moves.copy()
+		n.append("D")
+		new_state = Unsolved_Rubiks(new_matrix, n)
 		return new_state
 	def D_inv(self):
 		for n in range(0, 3, 1):
@@ -211,16 +220,39 @@ def Charles_Rubiks():
 	moved = []
 	moved.append(Unsolved_Rubiks(unsolved_state, []))
 	print(moved[0])
-	L = []
 	for l in yet_moves:
 		moved.append(l(moved[0]))
-	import sys
 	for e in moved:
-		if e.is_solved() == True:
-			break
+#		if e.is_solved() == True:
+#			break
 		print("State: {}, moves: {}".format(e, e.moves))
+		
+# Getting there
 	
+"""
+Left: ['R', 'R', 'R', 'R'] Front: ['G', 'G', 'G', 'G'] Right: ['O', 'O', 'O', 'O'] Back: ['B', 'B', 'B', 'B'] Up: ['Y', 'Y', 'Y', 'Y'] Down: ['W', 'W', 'W', 'W']
+State: Left: ['R', 'R', 'R', 'R'] Front: ['G', 'G', 'G', 'G'] Right: ['O', 'O', 'O', 'O'] Back: ['B', 'B', 'B', 'B'] Up: ['Y', 'Y', 'Y', 'Y'] Down: ['W', 'W', 'W', 'W'], moves: []
+State: Left: ['R', 'R', 'R', 'R'] Front: ['G', 'W', 'G', 'W'] Right: ['O', 'O', 'O', 'O'] Back: ['Y', 'B', 'Y', 'B'] Up: ['Y', 'G', 'Y', 'G'] Down: ['W', 'B', 'W', 'B'], moves: ['R']
+State: Left: ['R', 'R', 'R', 'R'] Front: ['G', 'W', 'G', 'W'] Right: ['O', 'O', 'O', 'O'] Back: ['Y', 'B', 'Y', 'B'] Up: ['Y', 'G', 'Y', 'G'] Down: ['W', 'B', 'W', 'B'], moves: ['R']
+State: Left: ['R', 'R', 'R', 'R'] Front: ['G', 'W', 'G', 'W'] Right: ['O', 'O', 'O', 'O'] Back: ['Y', 'B', 'Y', 'B'] Up: ['Y', 'G', 'Y', 'G'] Down: ['W', 'B', 'W', 'B'], moves: ['R']
+State: Left: ['R', 'R', 'R', 'R'] Front: ['W', 'G', 'W', 'G'] Right: ['O', 'O', 'O', 'O'] Back: ['B', 'Y', 'B', 'Y'] Up: ['G', 'Y', 'G', 'Y'] Down: ['B', 'W', 'B', 'W'], moves: ['L']
+State: Left: ['R', 'R', 'R', 'R'] Front: ['W', 'G', 'W', 'G'] Right: ['O', 'O', 'O', 'O'] Back: ['B', 'Y', 'B', 'Y'] Up: ['G', 'Y', 'G', 'Y'] Down: ['B', 'W', 'B', 'W'], moves: ['L']
+State: Left: ['R', 'R', 'R', 'R'] Front: ['W', 'G', 'W', 'G'] Right: ['O', 'O', 'O', 'O'] Back: ['B', 'Y', 'B', 'Y'] Up: ['G', 'Y', 'G', 'Y'] Down: ['B', 'W', 'B', 'W'], moves: ['L']
+State: Left: ['R', 'W', 'R', 'W'] Front: ['G', 'G', 'G', 'G'] Right: ['Y', 'O', 'Y', 'O'] Back: ['B', 'B', 'B', 'B'] Up: ['Y', 'Y', 'R', 'R'] Down: ['O', 'O', 'W', 'W'], moves: ['F']
+State: Left: ['R', 'W', 'R', 'W'] Front: ['G', 'G', 'G', 'G'] Right: ['Y', 'O', 'Y', 'O'] Back: ['B', 'B', 'B', 'B'] Up: ['Y', 'Y', 'R', 'R'] Down: ['O', 'O', 'W', 'W'], moves: ['F']
+State: Left: ['R', 'W', 'R', 'W'] Front: ['G', 'G', 'G', 'G'] Right: ['Y', 'O', 'Y', 'O'] Back: ['B', 'B', 'B', 'B'] Up: ['Y', 'Y', 'R', 'R'] Down: ['O', 'O', 'W', 'W'], moves: ['F']
+State: Left: ['W', 'R', 'W', 'R'] Front: ['G', 'G', 'G', 'G'] Right: ['O', 'Y', 'O', 'Y'] Back: ['B', 'B', 'B', 'B'] Up: ['R', 'R', 'Y', 'Y'] Down: ['W', 'W', 'O', 'O'], moves: ['B']
+State: Left: ['W', 'R', 'W', 'R'] Front: ['G', 'G', 'G', 'G'] Right: ['O', 'Y', 'O', 'Y'] Back: ['B', 'B', 'B', 'B'] Up: ['R', 'R', 'Y', 'Y'] Down: ['W', 'W', 'O', 'O'], moves: ['B']
+State: Left: ['W', 'R', 'W', 'R'] Front: ['G', 'G', 'G', 'G'] Right: ['O', 'Y', 'O', 'Y'] Back: ['B', 'B', 'B', 'B'] Up: ['R', 'R', 'Y', 'Y'] Down: ['W', 'W', 'O', 'O'], moves: ['B']
+State: Left: ['B', 'B', 'R', 'R'] Front: ['R', 'R', 'G', 'G'] Right: ['G', 'G', 'O', 'O'] Back: ['O', 'O', 'B', 'B'] Up: ['Y', 'Y', 'Y', 'Y'] Down: ['W', 'W', 'W', 'W'], moves: ['U']
+State: Left: ['B', 'B', 'R', 'R'] Front: ['R', 'R', 'G', 'G'] Right: ['G', 'G', 'O', 'O'] Back: ['O', 'O', 'B', 'B'] Up: ['Y', 'Y', 'Y', 'Y'] Down: ['W', 'W', 'W', 'W'], moves: ['U']
+State: Left: ['B', 'B', 'R', 'R'] Front: ['R', 'R', 'G', 'G'] Right: ['G', 'G', 'O', 'O'] Back: ['O', 'O', 'B', 'B'] Up: ['Y', 'Y', 'Y', 'Y'] Down: ['W', 'W', 'W', 'W'], moves: ['U']
+State: Left: ['R', 'R', 'B', 'B'] Front: ['G', 'G', 'R', 'R'] Right: ['O', 'O', 'G', 'G'] Back: ['B', 'B', 'O', 'O'] Up: ['Y', 'Y', 'Y', 'Y'] Down: ['W', 'W', 'W', 'W'], moves: ['D']
+State: Left: ['R', 'R', 'B', 'B'] Front: ['G', 'G', 'R', 'R'] Right: ['O', 'O', 'G', 'G'] Back: ['B', 'B', 'O', 'O'] Up: ['Y', 'Y', 'Y', 'Y'] Down: ['W', 'W', 'W', 'W'], moves: ['D']
+State: Left: ['R', 'R', 'B', 'B'] Front: ['G', 'G', 'R', 'R'] Right: ['O', 'O', 'G', 'G'] Back: ['B', 'B', 'O', 'O'] Up: ['Y', 'Y', 'Y', 'Y'] Down: ['W', 'W', 'W', 'W'], moves: ['D']
 
+[Program finished]
+""" 
 Charles_Rubiks()
 
 # Thank you Byron Bay Woolworths and Australia Post, could not have a sanitized home or meals on wheels without you
