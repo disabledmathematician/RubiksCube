f5a539aa183883920d36c2ee1fbc23809a8295fa
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2024-09-18 07:27:34 +1000

    Add files via upload

--- TruscottWatters.py
+++ TruscottWatters.py
@@ -1,251 +1,247 @@
-#pylint:disable=W0612
-# I love you, My father Mark William Watters
-# Thank you Harvard University and Massachusetts Institute of Technology
-# Charles Truscott Watters, Byron Bay Australia
-# Certified in Computational Thinking using Python from MITx
-# Certified in Data Science in Using Python for Research PH556 (Harvard Centre for Continuing Education, Harvard Extension School)
-# Love you high rollin Dad, Mark William Watters
-# Not finished yet. Need to get off my phone, pesky stalkerware malware, Android c&C
-# Gamma 6 for all possible states, will stochastically and combinatorially allow for 6! possible face directions, but that is implausible as faces are arranged in opposite settings
-import sys
-class Unsolved_Rubiks:
-	def __init__(self, state, moves):
-		self.state = state
-		self.moves = moves
-	def is_solved(self):
-		solved_state = [["R", "R", "R", "R"], ["G", "G", "G", "G"], ["O", "O", "O", "O"], ["B", "B", "B", "B"], ["Y", "Y", "Y", "Y"], ["W", "W", "W", "W"]]
-		if self.state == solved_state:
-			print("State: {} Solved by: {}".format(self.state, self.moves))
-			return True
-		return False
-		sys.exit()
-			
-	def R(self):
-		tL = self.state[0].copy()
-		tF = self.state[1].copy()
-		tR = self.state[2].copy()
-		tB = self.state[3].copy()
-		tU = self.state[4].copy()
-		tD = self.state[5].copy()
-		
-		prev_L = tL.copy()
-		prev_R = tR.copy()
-		prev_F = tF.copy()
-		prev_B = tB.copy()
-		prev_U = tU.copy()
-		prev_D = tD.copy()
-		tR[0], tR[1], tR[2], tR[3] = prev_R[2], prev_R[0], prev_R[3], prev_R[1]
-		tU[1], tU[3] = prev_F[1], prev_F[3]
-		tF[1], tF[3] = prev_D[1], prev_D[3]
-		tD[1], tD[3] = prev_B[0], prev_B[1]
-		tB[0], tB[2] = prev_U[3], prev_U[1]
-		new_matrix = [tL, tF, tR, tB, tU, tD]
-		n = self.moves.copy()
-#		self.moves.append("R")
-		n.append("R")
-		new_state = Unsolved_Rubiks(new_matrix, n)
-		return new_state
-	def R_inv(self):
-		raise NotImplementedError
-#		new_state = self.R()
-#		new_state = new_state.R()
-#		new_state = new_state.R()
-#		new_state.moves.pop()
-#		new_state.moves.pop()
-#		new_state.moves.pop()
-#		new_state.moves.append("R inverse")
-#		return new_state
-	def R2(self):
-		raise NotImplementedError
-	def L(self):
-		tL = self.state[0].copy()
-		tF = self.state[1].copy()
-		tR = self.state[2].copy()
-		tB = self.state[3].copy()
-		tU = self.state[4].copy()
-		tD = self.state[5].copy()
-		prev_L = tL.copy()
-		prev_R = tR.copy()
-		prev_F = tF.copy()
-		prev_B = tB.copy()
-		prev_U = tU.copy()
-		prev_D = tD.copy()
-		tL[0], tL[1], tL[2], tL[3] = prev_L[1], prev_L[3], prev_L[0], prev_L[2]
-		tF[0], tF[2] = prev_D[0], prev_D[2]
-		tB[1], tB[3] = prev_U[2], prev_U[0]
-		tU[0], tU[2] = prev_F[0], prev_F[2]
-		tD[0], tD[2] = prev_B[3], prev_B[1]
-		new_matrix = [tL, tF, tR, tB, tU, tD]
-		n = self.moves.copy()
-		n.append("L")
-#		self.moves.append("L")
-		new_state = Unsolved_Rubiks(new_matrix, n)
-		return new_state
-	def L_inv(self):
-		raise NotImplementedError
-	def L2(self):
-		raise NotImplementedError
-	def F(self):
-		tL = self.state[0].copy()
-		tF = self.state[1].copy()
-		tR = self.state[2].copy()
-		tB = self.state[3].copy()
-		tU = self.state[4].copy()
-		tD = self.state[5].copy()
-		
-		prev_L = tL.copy()
-		prev_R = tR.copy()
-		prev_F = tF.copy()
-		prev_B = tB.copy()
-		prev_U = tU.copy()
-		prev_D = tD.copy()
-		tF[0], tF[1], tF[2], tF[3] = prev_F[2], prev_F[0], prev_F[3], prev_F[1]
-		tU[2], tU[3] = prev_L[3], prev_L[1]
-		tL[1], tL[3] = prev_D[0], prev_D[1]
-		tD[0], tD[1] = prev_R[2], prev_R[0]
-		tR[2], tR[0] = prev_U[3], prev_U[2]
-		new_matrix = [tL, tF, tR, tB, tU, tD]
-		n = self.moves.copy()
-		n.append("F")
-#		self.moves.append("F")
-		new_state = Unsolved_Rubiks(new_matrix, n)
-		return new_state
-	def F_inv(self):
-		raise NotImplementedError
-	def F2(self):
-		raise NotImplementedError
-	def B(self):
-		tL = self.state[0].copy()
-		tF = self.state[1].copy()
-		tR = self.state[2].copy()
-		tB = self.state[3].copy()
-		tU = self.state[4].copy()
-		tD = self.state[5].copy()
-	
-		prev_L = tL.copy()
-		prev_R = tR.copy()
-		prev_F = tF.copy()
-		prev_B = tB.copy()
-		prev_U = tU.copy()
-		prev_D = tD.copy()
-		tB[0], tB[1], tB[2], tB[3] = prev_B[2], prev_B[0], prev_B[3], prev_B[1]
-		tU[0], tU[1] = prev_L[2], prev_L[0]
-		tL[0], tL[2] = prev_D[2], prev_D[3]
-		tD[2], tD[3] = prev_R[3], prev_R[1]
-		tR[1], tR[3] = prev_U[0], prev_U[1]
-		new_matrix = [tL, tF, tR, tB, tU, tD]
-		n = self.moves.copy()
-		n.append("B")
-#		self.moves.append("B")
-		new_state = Unsolved_Rubiks(new_matrix, n)
-		return new_state
-	def B_inv(self):
-		raise NotImplementedError
-	def B2(self):
-		raise NotImplementedError
-	def U(self):
-		tL = self.state[0].copy()
-		tF = self.state[1].copy()
-		tR = self.state[2].copy()
-		tB = self.state[3].copy()
-		tU = self.state[4].copy()
-		tD = self.state[5].copy()
-		
-		prev_L = tL.copy()
-		prev_R = tR.copy()
-		prev_F = tF.copy()
-		prev_B = tB.copy()
-		prev_U = tU.copy()
-		prev_D = tD.copy()
-		tU[0], tU[1], tU[2], tU[3] = prev_U[2], prev_U[0], prev_U[3], prev_U[1]
-		tL[0], tL[1] = prev_F[0], prev_F[1]
-		tB[0], tB[1] = prev_L[0], prev_L[1]
-		tR[0], tR[1] = prev_B[0], prev_B[1]
-		tF[0], tF[1] = prev_R[0], prev_R[1]
-		new_matrix = [tL, tF, tR, tB, tU, tD]
-		n = self.moves.copy()
-		n.append("U")
-#		self.moves.append("U")
-		new_state = Unsolved_Rubiks(new_matrix, n)
-		return new_state
-	def U2(self):
-		raise NotImplementedError
-	def U_inv(self):
-		raise NotImplementedError
-	def D(self):
-		tL = self.state[0].copy()
-		tF = self.state[1].copy()
-		tR = self.state[2].copy()
-		tB = self.state[3].copy()
-		tU = self.state[4].copy()
-		tD = self.state[5].copy()
-		
-		prev_L = tL.copy()
-		prev_R = tR.copy()
-		prev_F = tF.copy()
-		prev_B = tB.copy()
-		prev_U = tU.copy()
-		prev_D = tD.copy()
-		tD[0], tD[1], tD[2], tD[3] = prev_D[2], prev_D[0], prev_D[3], prev_D[1]
-		tL[2], tL[3] = prev_B[2], prev_B[3]
-		tB[2], tB[3] = prev_R[2], prev_R[3]
-		tR[2], tR[3] = prev_F[2], prev_F[3]
-		tF[2], tF[3] = prev_L[2], prev_L[3]
-		new_matrix = [tL, tF, tR, tB, tU, tD]
-		n = self.moves.copy()
-		n.append("D")
-#		self.moves.append("D")
-		new_state = Unsolved_Rubiks(new_matrix, n)
-		return new_state
-	def D_inv(self):
-		raise NotImplementedError
-	def D2(self):
-		raise NotImplementedError
-	def __str__(self):
-		return str("Left: {} Front: {} Right: {} Back: {} Up: {} Down: {}".format(self.state[0], self.state[1], self.state[2], self.state[3], self.state[4], self.state[5]))
+#pylint:disable=W0613
+from queue import deque
 
-def Charles_Rubiks():
-	solved_state = [["R", "R", "R", "R"], ["G", "G", "G", "G"], ["O", "O", "O", "O"], ["B", "B", "B", "B"], ["Y", "Y", "Y", "Y"], ["W", "W", "W", "W"]]
-	ns = Unsolved_Rubiks(solved_state, [])
-	ns1 = ns.R()
-	ns2 = ns.L()
-	ns = ns.R()
-	ns = ns.L()
-	print("ns")
-	print(ns)
-	print(ns.R(), ns.L())
-	my_cube = [["B", "R", "B", "Y"], ["W", "B", "G", "G"], ["O", "O", "Y", "R"], ["G", "O", "W", "R"], ["Y", "W", "G", "W"], ["R", "O", "Y", "B"]]
-	state = Unsolved_Rubiks(my_cube, [])
-#	noviy = ns.state.copy()
-	yet_moves =[lambda c: c.L(), lambda c: c.R(), lambda c: c.U(), lambda c: c.D(), lambda c: c.F(), lambda c: c.B()]
-#	Can't solve my cube just as of yet
-	States = []
-	States.append(state)
-	print(state.state, state.moves)
-	for l in yet_moves:
-		n = l(state)
-		print(n, n.moves)
-		States.append(n)
-	print(States)
-	t = False
-	x = 1
-	while t == False:
-		while x <= len(States):
-			f = States[x]
-			for l in yet_moves:
-				mov = l(f)
-				States.append(mov)
-				if mov.is_solved() == True:
-					print("{} Solved by {}".format(ns.state, mov.moves))
-					t = True
-			x += 1
-	try:
-		Unsolved_Rubiks([["R", "R", "R", "R"], ["G", "G", "G", "G"], ["O", "O", "O", "O"], ["B", "B", "B", "B"], ["Y", "Y", "Y", "Y"], ["W", "W", "W", "W"]], [])
-	except NotImplementedError as e:
-		print(e)
-#	for e in States:
-#		print(e, e.moves)
-""" I love you, my precious Father Mark William Watters. I love you unconditionally
-All the credit for Tai as our carer
-""" 
-Charles_Rubiks()
+# August 30, International Day of the Disappeared
+
+""" Charles Truscott Watters, developing my own algorithm to solve the Rubik's cube 2 x 2 Byron Bay NSW 2481. Thank you John Flynn Hospital and Byron Bay Hospital. Thank you MITx, MIT OCW and Harvard CCE """
+
+""" Thank you Byron Central Hospital Tuckeroo. Thank you Eric Grimson, John Guttag and Ana Bell and all at MITx """
+
+
+#  Theta c to the n
+
+""" Top Left Front, Top Right Front, Bottom Left Front, Bottom Right Front, Top Left Back, Top Right Back, Bottom Right Back, Bottom Left Back """
+class RubiksState(object):
+	def __init__(self, tlf, trf, blf, brf, tlb, blb, trb, brb, moves):
+#	def __init__(self, left_face, front_face, right_face, back_face, top_face, down_face, moves):
+#		self.tlf = [0] * 3
+#		self.trf= [0] * 3
+#		self.blf = [0] * 3
+#		self.brf= [0] * 3
+#		self.tlb = [0] * 3
+#		self.trb= [0] * 3
+#		self.brb = [0] * 3
+#		self.blb= [0] * 3
+#		
+		self.tlf = tlf
+		self.trf = trf
+		self.blf = blf
+		self.brf = brf
+		self.tlb = tlb
+		self.blb = blb
+		self.trb = trb
+		self.brb = brb
+
+		self.left_face = [self.tlf[1], self.tlb[1], self.blb[1], self.blf[1]]
+		self.front_face = [self.tlf[2], self.blf[2], self.trf[2], self.brf[2]]
+		self.right_face = [self.trf[1], self.trb[1], self.brf[1], self.brb[1]]
+		self.back_face = [self.tlb[2], self.trb[2], self.blb[2], self.brb[2]]
+		self.top_face = [self.tlb[0], self.trb[0], self.tlf[0], self.trf[0]]
+		self.down_face = [self.blb[0], self.brb[0], self.blf[0], self.brf[0]]
+		self.moves = moves
+
+	def L(self):
+		""" Top Left Front to Bottom Left Front.
+Bottom Left Front to Bottom Left Back
+Bottom Left Back to Top Left Back
+Top Left Back to Top Left Front """
+# Aaaah, Erroneous decision 
+	# Indices 0, 1, 2 to 2, 1, 0 (mapping)
+		ttlf = self.tlf.copy()
+		tblf = self.blf.copy()
+		ttrf = self.trf.copy()
+		tbrf = self.brf.copy()
+		ttlb = self.tlb.copy()
+		tblb = self.blb.copy()
+		ttrb = self.trb.copy()
+		tbrb = self.brb.copy()
+		nblf = [0] * 3
+		nblb = [0] * 3
+		ntlb = [0] * 3
+		ntlf = [0] * 3
+		nblf[2], nblf[1], nblf[0] = ttlf[0], ttlf[1], ttlf[2] # Bottom Left Front becomes Top Left Front
+		nblb[2], nblb[1], nblb[0] = tblf[0], tblf[1], tblf[2] # Bottom Left Back becomes Bottom Left Front
+		ntlb[2], ntlb[1], ntlb[0] = tblb[0], tblb[1], tblb[2] #  Top Left Back becomes Bottom Left Back
+		ntlf[2], ntlf[1], ntlf[0] = ttlb[0], ttlb[1], ttlb[2]
+		# Top Left Front becomes Top Left Back
+		elcopy = self.moves.copy()
+		elcopy.append("L")
+		# tlf, trf, blf, brf, tlb, blb, trb, brb, moves):
+		return RubiksState(ntlf.copy(), ttrf.copy(), nblf.copy(), tbrf.copy(), ntlb.copy(), nblb.copy(), ttrb.copy(), tbrb.copy(), elcopy)
+	def R(self):
+		# Indices 0, 1, 2 to 2, 1, 0 (mapping)
+		""" Top Right Front to Bottom Right Front.
+	Bottom Right Front to Bottom Right Back
+	Bottom Right Back to Top Right Back
+	Top Right Back to Top Right Front """
+		ttlf = self.tlf.copy()
+		tblf = self.blf.copy()
+		ttrf = self.trf.copy()
+		tbrf = self.brf.copy()
+		ttlb = self.tlb.copy()
+		tblb = self.blb.copy()
+		ttrb = self.trb.copy()
+		tbrb = self.brb.copy()
+		nbrf, nbrb, ntrb, ntrf = [0] * 3, [0] * 3, [0] * 3,[0] * 3
+		nbrf[2], nbrf[1], nbrf[0] = ttrf[0], ttrf[1], ttrf[2]
+		nbrb[2], nbrb[1], nbrb[0] = tbrf[0], tbrf[1], tbrf[2]
+		ntrb[2], ntrb[1], ntrb[0] = tbrb[0], tbrb[1], tbrb[2]
+		ntrf[2], ntrf[1], ntrf[0] = ttrb[0], ttrb[1], ttrb[2]
+		elcopy = self.moves.copy()
+		elcopy.append("R")
+		return RubiksState(ttlf.copy(), ntrf.copy(), tblf.copy(), nbrf.copy(), ttlb.copy(), tblb.copy(), ntrb.copy(), nbrb.copy(), elcopy)
+	def U(self):
+		# Indices 0, 1, 2 to 0, 2, 1 (mapping)
+		""" Top Left Front to Top Left Back
+	Top Left Back to Top Right Back. Top Right Back to Top Right Front. Top Right Front to Top Left Front """
+		ttlf = self.tlf.copy()
+		tblf = self.blf.copy()
+		ttrf = self.trf.copy()
+		tbrf = self.brf.copy()
+		ttlb = self.tlb.copy()
+		tblb = self.blb.copy()
+		ttrb = self.trb.copy()
+		tbrb = self.brb.copy()
+		ntlb = [0] * 3
+		ntrb = [0] * 3
+		ntrf = [0] * 3
+		ntlf = [0] * 3
+		ntlb[0], ntlb[2], ntlb[1] = ttlf[0], ttlf[1], ttlf[2]
+		ntrb[0], ntrb[2], ntrb[1] = ttlb[0], ttlb[1], ttlb[2]
+		ntrf[0], ntrf[2], ntrf[1] = ttrb[0], ttrb[1], ttrb[2]
+		ntlf[0], ntlf[2], ntlf[1] = ttrf[0], ttrf[1], ttrf[2]
+		
+		elcopy = self.moves.copy()
+		elcopy.append("U")
+		return RubiksState(ntlf.copy(), ntrf.copy(), tblf.copy(), tbrf.copy(), ntlb.copy(), tblb.copy(), ntrb.copy(), tbrb.copy(), elcopy)
+	def D(self):
+		# Indices 0, 1, 2 to 0, 2, 1 (mapping)
+		""" Bottom Left Front to Bottom Left Back
+		Bottom Left Back to Bottom Right Back. Bottom Right Back to Bottom Right Front
+		Bottom Right Front to Bottom Left Front """
+		ttlf = self.tlf.copy()
+		tblf = self.blf.copy()
+		ttrf = self.trf.copy()
+		tbrf = self.brf.copy()
+		ttlb = self.tlb.copy()
+		tblb = self.blb.copy()
+		ttrb = self.trb.copy()
+		tbrb = self.brb.copy()
+		nblb, nbrb, nbrf, nblf= [0] * 3, [0] * 3, [0] * 3, [0] * 3
+		nblb[0], nblb[2], nblb[1] = tblf[0], tblf[1], tblf[2]
+		nbrb[0], nbrb[2], nbrb[1] = tblb[0], tblb[1], tblb[2]
+		nbrf[0], nbrf[2], nbrf[1] = tbrb[0], tbrb[1], tbrb[2]
+		nblf[0], nblf[2], nblf[1] = tbrf[0], tbrf[1], tbrf[2]
+		elcopy = self.moves.copy()
+		elcopy.append("D")
+		return RubiksState(ttlf.copy(), ttrf.copy(), nblf.copy(), nbrf.copy(), ttlb.copy(), nblb.copy(), ttrb.copy(), nbrb.copy(), elcopy)
+		pass
+	def F(self):
+		# Indices 0, 1, 2 to 1, 0, 2 (mapping)
+		""" Bottom Left Front to Top Left Front. Top Left Front to Top Right Front. Top Right Front to Bottom Right Front. Bottom Right Front to Bottom Left Front """
+		ttlf = self.tlf.copy()
+		tblf = self.blf.copy()
+		ttrf = self.trf.copy()
+		tbrf = self.brf.copy()
+		ttlb = self.tlb.copy()
+		tblb = self.blb.copy()
+		ttrb = self.trb.copy()
+		tbrb = self.brb.copy()
+		ntlf, ntrf, nbrf, nblf = [0] * 3, [0] * 3, [0] * 3, [0] * 3
+		ntlf[1], ntlf[0], ntlf[2] = tblf[0], tblf[1], tblf[2]
+		ntrf[1], ntrf[0], ntrf[2] = ttlf[0], ttlf[1], ttlf[2]
+		nbrf[1], nbrf[0], nbrf[2] = ttrf[0], ttrf[1], ttrf[2]
+		nblf[1], nblf[0], nblf[2] = tbrf[0], tbrf[1], tbrf[2]
+		elcopy = self.moves.copy()
+		elcopy.append("F")
+		return RubiksState(ntlf.copy(), ntrf.copy(), nblf.copy(), nbrf.copy(), ttlb.copy(), tblb.copy(), ttrb.copy(), tbrb.copy(), elcopy)
+
+	def B(self):
+		# Indices 0, 1, 2 to 1, 0, 2 (mapping)
+		""" Bottom Left Back to Top Left Back. Top Left Back to Top Right Back. Top Right Back to Bottom Right Back. Bottom Right Back to Bottom Left Back """
+		ttlf = self.tlf.copy()
+		tblf = self.blf.copy()
+		ttrf = self.trf.copy()
+		tbrf = self.brf.copy()
+		ttlb = self.tlb.copy()
+		tblb = self.blb.copy()
+		ttrb = self.trb.copy()
+		tbrb = self.brb.copy()
+		ntlb, ntrb, nbrb, nblb = [0] * 3, [0] * 3, [0] * 3, [0] * 3
+		ntlb[1], ntlb[0], ntlb[2] = tblb[0], tblb[1], tblb[2]
+		ntrb[1], ntrb[0], ntrb[2] = ttlb[0], ttlb[1], ttlb[2]
+		nbrb[1], nbrb[0], nbrb[2] = ttrb[0], ttrb[1], ttrb[2]
+		nblb[1], nblb[0], nblb[2] = tbrb[0], tbrb[1], tbrb[2]
+		elcopy = self.moves.copy()
+		elcopy.append("B")
+		return RubiksState(ttlf.copy(), ttrf.copy(), tblf.copy(), tbrf.copy(), ntlb.copy(), nblb.copy(), ntrb.copy(), nbrb.copy(), elcopy)
+#		return RubiksState(elcopy)
+#	def B2(self):
+#		elcopy = self.moves.copy()
+#		elcopy.append("B2")
+#		return RubiksState(elcopy)
+#		pass
+#	def Binv(self):
+#		elcopy = self.moves.copy()
+#		elcopy.append("B inverse")
+#		return RubiksState(elcopy)
+#		pass
+		
+	def is_solved(self):
+		if self.left_face == ["O", "O", "O", "O"] and self.front_face == ["G", "G", "G", "G"] and self.right_face == ["R", "R", "R", "R"] and self.back_face == ["B", "B", "B", "B"] and self.top_face == ["W", "W", "W", "W"] and self.down_face == ["Y", "Y" , "Y" , "Y"]:
+			print("Solved")
+			return True
+			exit(1)
+		elif self.right_face == ["O", "O", "O", "O"] and self.front_face == ["G", "G", "G", "G"] and self.left_face == ["R", "R", "R", "R"] and self.back_face == ["B", "B", "B", "B"] and self.top_face == ["W", "W", "W", "W"] and self.down_face == ["Y", "Y" , "Y" , "Y"]:
+			print("Solved")
+			return True
+			exit(1)
+		elif self.left_face == ["O", "O", "O", "O"] and self.back_face == ["G", "G", "G", "G"] and self.right_face == ["R", "R", "R", "R"] and self.front_face == ["B", "B", "B", "B"] and self.top_face == ["W", "W", "W", "W"] and self.down_face == ["Y", "Y" , "Y" , "Y"]:
+			print("Solved")
+			return True
+			exit(1)
+		elif self.left_face == ["O", "O", "O", "O"] and self.front_face == ["G", "G", "G", "G"] and self.right_face == ["R", "R", "R", "R"] and self.back_face == ["B", "B", "B", "B"] and self.down_face == ["W", "W", "W", "W"] and self.top_face == ["Y", "Y" , "Y" , "Y"]:
+			print("Solved")
+			return True
+			exit(1)
+		else:
+			print("Not solved: {} {} {} {} {} {}".format(self.left_face, self.front_face, self.right_face, self.back_face, self.top_face, self.down_face))
+		return False
+
+
+def CharlesTruscottRubiks():
+#	item = RubiksState([])
+# tlf, trf, blf, brf, tlb, blb, trb, brb, moves):
+	item = RubiksState(["W", "B", "O"], ["W", "R", "G"], ["B", "Y", "R"], ["G", "O", "Y"], ["W", "G", "O"], ["G", "Y", "R"], ["W", "R", "B"], ["B", "O", "Y"], [])
+#	item = RubiksState(["W", "O", "G"], ["W", "R", "G"], ["Y", "O", "G"], ["Y", "R", "G"], ["W", "O", "B"], ["Y", "O", "B"], ["W", "R", "B"], ["Y", "R", "B"], [])
+	state = deque([])
+	state.append(item)
+	moves = [lambda s: s.L(), lambda s: s.R(), lambda s: s.U(), lambda s: s.D(), lambda s: s.F(),  lambda s: s.B() ]
+#	moves = [lambda s: s.L(), lambda s: s.L2(), lambda s: s.Linv(), lambda s: s.R(), lambda s: s.R2(), lambda s: s.Rinv(), lambda s: s.U(), lambda s: s.U2(), lambda s: s.Uinv(), lambda s: s.D(), lambda s: s.D2(), lambda s: s.Dinv(), lambda s: s.F(), lambda s: s.F2(), lambda s: s.Finv(), lambda s: s.B(), lambda s: s.B2(), lambda s: s.Binv()]
+#	print(state[0].moves)
+	for move in moves:
+		e = move(item)
+		state.append(e)
+	state.popleft()
+	c = 0
+	print(state)
+	print("Charles Truscott Watters. My Rubik's cube solution Python algorithm.")
+	from time import sleep
+	while c < 6 ** 32:
+		elem = state.popleft()
+		for move in moves:
+			t = move(elem)
+			state.append(t)
+			print(t, t.moves)
+			print("Left Face: {} Front Face: {} Right Face: {} Back Face: {} Top Face: {} Down Face: {}".format(t.left_face, t.front_face, t.right_face, t.back_face, t.top_face, t.down_face))
+			print("Authored by Charles Truscott Watters. Rubik's algorithm")
+#			sleep(0.2)
+		if elem.is_solved() == True:
+			print("Answer: ")
+			print(elem.moves)
+			exit(1)
+		c += 1
+
+
+
+CharlesTruscottRubiks()
